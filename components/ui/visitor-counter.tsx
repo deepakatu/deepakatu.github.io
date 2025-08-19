63688dd62d8af78addaa00ce4f32527e7d1567b3
Deepak Manoharan <deepakmanonz@gmail.com>
2025-08-19 15:33:21 +1200

    updated

--- components/ui/visitor-counter.tsx
+++ components/ui/visitor-counter.tsx
@@ -3,7 +3,7 @@ export default function VisitorCounter() {
   return (
     <div className="flex justify-center mt-4">
       <img
-        src="https://hitwebcounter.com/counter/counter.php?page=21427903&style=0008&nbdigits=5&type=page&initCount=0"
+        src="https://hitwebcounter.com/counter/counter.php?page=21427923&style=0008&nbdigits=5&type=ip&initCount=0"
         alt="Visit counter For Websites"
         title="Counter Widget"
         style={{ border: 0 }} // borderless via CSS, NOT attribute
