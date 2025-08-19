c1166e13c69deb38d831a1e983bf1f1d04ff4547
Deepak Manoharan <deepakmanonz@gmail.com>
2025-08-19 14:12:27 +1200

    changes

--- components/ui/visitor-counter.tsx
+++ components/ui/visitor-counter.tsx
@@ -1,13 +1,13 @@
 "use client";
 export default function VisitorCounter() {
   return (
-    <div style={{ margin: "2rem 0", textAlign: "center" }}>
+    <div style={{ margin: "1rem 0", textAlign: "center" }}>
       <a href="https://www.hitwebcounter.com" target="_blank" rel="noopener noreferrer">
         <img
           src="https://hitwebcounter.com/counter/counter.php?page=21427484&style=0001&nbdigits=5&type=page&initCount=0"
           alt="Visit counter For Websites"
           title="Counter Widget"
-          border="0"
+          style={{ border: 0 }} // borderless via CSS, NOT attribute
         />
       </a>
     </div>
